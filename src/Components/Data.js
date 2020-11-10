22fe75a0b46c0ff3797b869b47bf1ac7062314da
Matari Howard <howardmatari@gmail.com>
2020-11-09 23:09:21 -0500

    Ascending first name

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,28 +1,46 @@
-import React, {Component} from 'react';
-import api from '../utils/api';
-import Table from './Table';
+import React, { Component } from "react";
+import api from "../utils/api";
+import Table from "./Table";
 
 class Data extends Component {
+  state = {
+    users: [],
+    order: "ascend",
+    sortUser: []
+  };
+  componentDidMount() {
+    api.randomUsers().then((results) => {
+      this.setState({
+        users: results.data.results,
+      });
+      console.log(results);
+      console.log(this.state.users);
+    });
+  }
 
-    state = {
-        users: []
+  //if ascend .C
+  //if descend .sort((a,b) => (b.name.first - a.name.first))
+  handleSort = (name) => {
+    if (this.state.order === "ascend") {
+      this.setState({users:this.state.users.sort((a,b) => (b.name.first - a.name.first))})
+      this.setState({
+        order: "descend",
+       
+      })
+      console.log("descend");
+    } else {
+      this.setState({
+        order: "ascend",
+      })
+      console.log("ascend");
     }
-    componentDidMount() {
-        api.randomUsers().then(results => {
-            this.setState({
-                users: results.data.results
-            })
-            console.log(results);
-            console.log(this.state.users)
-        })
-    }
-
-render() {
-    return (
-       <Table users={this.state.users} />
-    )
-}
-
+  };
+  render() {
+    return <Table 
+    users={this.state.users}
+    order={this.state.order}
+     />;
+  }
 }
 
 export default Data;
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,15 +1,80 @@
 import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleUp, faAngleDown} from "@fortawesome/free-solid-svg-icons";
+import {Component} from 'react';
+import api from "../utils/api";
 
-function Table({ users }) {
+
+
+class Table extends Component {
+    state = {
+        users: [],
+        order: "ascend",
+        sortUser: []
+    }
+
+
+    componentDidMount() {
+        api.randomUsers().then((results) => {
+          this.setState({
+            users: results.data.results,
+          });
+          console.log(results);
+          console.log(this.state.users);
+        });
+      }
+    handleSort = (name) => {
+        if (this.state.order === "ascend") {
+          this.setState({users:this.state.users.sort((a,b) => b.name.first > a.name.first? 1:-1)})
+          this.setState({
+            order: "descend",
+           
+          })
+          console.log("descend");
+        } else {
+            this.setState({users:this.state.users.sort((a,b) => b.name.first < a.name.first? 1:-1)})
+          this.setState({
+            order: "ascend",
+          })
+          console.log("ascend");
+        }
+      };
+
+      handleSort = (email) => {
+        if (this.state.order === "ascend") {
+          this.setState({users:this.state.users.sort((a,b) => b.email > a.email? 1:-1)})
+          this.setState({
+            order: "descend",
+           
+          })
+          console.log("descend");
+        } else {
+            this.setState({users:this.state.users.sort((a,b) => b.email < a.email? 1:-1)})
+          this.setState({
+            order: "ascend",
+          })
+          console.log("ascend");
+        }
+      };
+
+
+      render(){
+
+      
     return (
         <table>
         <thead>
         <td>Image</td>
-        <td> Name</td>
+        <td onClick={ this.handleSort}> Name
+        
+        {
+            this.state.order === "descend" ?  <FontAwesomeIcon icon={faAngleUp} /> :  <FontAwesomeIcon icon={faAngleDown} /> 
+        }
+        </td>
         <td>Email</td> 
         <td>Phone</td>
         </thead>
-        {users.map(({ name, login, picture, email, phone }) => {
+        {this.state.users.map(({ name, login, picture, email, phone }) => {
             return (
                 <tr key={phone}>
                 <td><img src={picture.medium}
@@ -22,7 +87,7 @@ function Table({ users }) {
             )
         })}
         </table>
-    )
+    )}
 }
 
 export default Table;
