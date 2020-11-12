9d5146b29d24ae20348f3eaa01b7e41dc5fc7587
Matari Howard <howardmatari@gmail.com>
2020-11-11 21:59:44 -0500

    Final Review

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -10,14 +10,25 @@ class Table extends Component {
     state = {
         users: [],
         order: "ascend",
-        sortUser: []
+        sortUser: [],
+        filterTerm: "",
+        filteredUsers:[]
+    }
+     
+    handleChange = (event) => {
+        this.setState({filterTerm: event.target.value});
+
     }
 
+    handleSubmit =() => {
+         const filteredUsers=this.state.users.filter(user=>user.name.first.includes(this.state.filterTerm))
+         this.setState({filteredUsers})
+    }
 
     componentDidMount() {
         api.randomUsers().then((results) => {
           this.setState({
-            users: results.data.results,
+            users: results.data.results, filteredUsers: results.data.results
           });
           console.log(results);
           console.log(this.state.users);
@@ -58,10 +69,24 @@ class Table extends Component {
       };
 
 
+    //   const checkUsernameFilter = user =>(
+    //     user.name
+    //       .toLowerCase()
+    //       .indexOf(this.state.filterUsers.toLowerCase()) !== -1
+
       render(){
 
       
     return (
+    
+        <div>
+        <label>
+          Name:
+          <input type="text" value={this.state.filterTerm} onChange={this.handleChange} />
+        </label>
+        <input type="button" onClick={this.handleSubmit} value="Submit" />
+        
+    
         <table>
         <thead>
         <td>Image</td>
@@ -74,7 +99,7 @@ class Table extends Component {
         <td>Email</td> 
         <td>Phone</td>
         </thead>
-        {this.state.users.map(({ name, login, picture, email, phone }) => {
+        {this.state.filteredUsers.map(({ name, login, picture, email, phone }) => {
             return (
                 <tr key={phone}>
                 <td><img src={picture.medium}
@@ -87,6 +112,7 @@ class Table extends Component {
             )
         })}
         </table>
+          </div>
     )}
 }
 
